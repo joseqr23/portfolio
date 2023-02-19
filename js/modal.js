1b6c159adca3dc052bcdae77a0f91c2d9bf82a15
José Leandro Quispe Reyes <113225090+joseqr23@users.noreply.github.com>
2023-02-18 19:09:54 -0500

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -96,7 +96,7 @@ img3.onclick = function () {
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
